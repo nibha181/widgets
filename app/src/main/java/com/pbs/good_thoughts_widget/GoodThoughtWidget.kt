0d1028445675a27fa29ec39bd2fb509bfed382ad
NibhaJ <nibha.jain@xtranetindia.com>
2023-10-17 18:22:01 +0530

    first commit

---
+++ app/src/main/java/com/pbs/good_thoughts_widget/GoodThoughtWidget.kt
@@ -0,0 +1,42 @@
+package com.pbs.good_thoughts_widget
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+class GoodThoughtWidget : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // Perform this loop procedure for each widget that belongs to this
+        // provider.
+        appWidgetIds.forEach { appWidgetId ->
+            // Create an Intent to launch ExampleActivity.
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(
+                /* context = */ context,
+                /* requestCode = */  0,
+                /* intent = */ Intent(context, MainActivity::class.java),
+                /* flags = */ PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            // Get the layout for the widget and attach an onClick listener to
+            // the button.
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.good_thought_widget
+            ).apply {
+                setOnClickPendingIntent(R.id.button, pendingIntent)
+            }
+
+            // Tell the AppWidgetManager to perform an update on the current
+            // widget.
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
